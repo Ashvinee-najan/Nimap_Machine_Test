a26d80e25a6ae4896c31db925d6943562eeedc63
unknown <ashvinee.najan@gmail.com>
2024-07-18 17:33:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import TopRated from './components/TopRated';
+import Upcoming from './components/Upcoming';
+import MovieDetails from './components/MovieDetails';
+import SearchResults from './components/SearchedResults';
+import './App.css';
+import { BrowserRouter as Router , Routes , Route} from 'react-router-dom';
+
+const App = () => (
+  <Router>
+ <Navbar/>
+ <Routes>
+ <Route path="/" exact element={<Home/>} />
+ <Route path="/top-rated" element={<TopRated/>} />
+  <Route path="/upcoming" element={<Upcoming/>} />
+  <Route path="/movie/:id" element={<MovieDetails/>} />
+  <Route path="/search" element={<SearchResults/>} />
+ </Routes>
+  </Router>
+
+);
 
 export default App;
